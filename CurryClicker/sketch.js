b1b5f944853768468201efc4f892c3c2337685fb
Salaad102 <salaara102@gmail.com>
2022-09-20 12:05:09 -0600

    Added Currybowl & Background

--- CurryClicker/sketch.js
+++ CurryClicker/sketch.js
@@ -6,17 +6,20 @@
 // - describe what you did to take this project "above and beyond"
 
 
-let bg;
-function preload(){
-  bg = loadImage("CurryClicker/Indian_Spices.jpg");
-}
 
+let landscape;
+let curryBowl;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(img, 0)
-  image(img, 0, 0);
+  background(100);
 }
 
 function draw() {
-  background(220);
+  imageMode(CENTER);
+  image(landscape, width/2, height/2, width, height);
 }
+
+function preload() {
+  landscape = loadImage("Indian_Spices.jpg");
+  curryBowl = loadImage("CurryBowl.png");
+} // Making the background an image
