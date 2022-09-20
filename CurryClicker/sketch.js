cc9033f38a394ccdfc72a7ae1fc31a12075fe479
Salaad102 <salaara102@gmail.com>
2022-09-20 11:46:02 -0600

    Added Chessboard & CurryClicker

---
+++ CurryClicker/sketch.js
@@ -0,0 +1,22 @@
+// Curry Clicker
+// Salaar Ahmed
+// Sept. 19, 2022
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+let bg;
+function preload(){
+  bg = loadImage("CurryClicker/Indian_Spices.jpg");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(img, 0)
+  image(img, 0, 0);
+}
+
+function draw() {
+  background(220);
+}
