9705ca73d178adf40c4c5160c904a65a6fba2b74
Salaad102 <salaara102@gmail.com>
2022-09-28 12:17:49 -0600

    idke

--- CurryClicker/sketch.js
+++ CurryClicker/sketch.js
@@ -20,6 +20,7 @@ let spoon = 0;
 let spoonx;
 let spoony;
 let spoonMultiplier;
+let currentTime;
 
 function preload() {
   landscape = loadImage("blackMarble.png"); // Making the background an image
@@ -27,12 +28,21 @@ function preload() {
   spoonIcon = loadImage("spoon2.png"); // Making a spoon icon
 } 
 
-function createCurry() { 
+function createCurry() { //random bowl of curry is made in a certain area
   image(curryBowl, random(curryBowlx, curryBowlx*2 - curryBowlx/2), random(curryBowlx, curryBowly), curryBowl.width*scalar, curryBowl.height*scalar);
 }
 
 function spoonModifier(spoon) {
+  if (spoon > 0){
+    score = score + currentTime;
+  }
+  
+}
 
+function timer() {
+  currentTime = int(millis() / 1000);
+  fill("white");
+  text("TIME: " + currentTime, width/2 - 75, height*0.05);
 }
 
 function setup() {
@@ -54,11 +64,10 @@ function draw() {
 
 
   textSize(45);
-  let currentTime = int(millis() / 1000);
-  fill("white");
-  text("TIME: " + currentTime, width/2 - 75, height*0.05);
+  timer();
   fill("white");
   text(score + " Curry", width/2 - 75, height*0.95); // text location in under curry bowl
+  spoonModifier(spoon);
 }
 
 function mouseClicked() { 
@@ -72,8 +81,7 @@ function keyPressed() {
   if (score >= 10)  {
     if (keyCode === 83){ // If score is greater than 10 and the s key is pressed, add a spoon and take away 10 curry.
       score = score - 10;
-      spoon = spoon + 1;
-      spoonMultiplier = 0;
+      spoon = spoon++;
     }
   }
 }
