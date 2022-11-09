4e78bbd1ccc0202e6e1900fe63159e8c5c2b04f6
Salaad102 <salaara102@gmail.com>
2022-11-09 12:17:10 -0600

    messing around like a brown monke

--- Moving Character with Grid/sketch.js
+++ Moving Character with Grid/sketch.js
@@ -6,7 +6,7 @@ let state = "startScreen";
 const ROWS = 12;
 const COLS = 24;
 let roadStrips = [];
-let velocity = 0;
+let velocity = 0.6;
 let grid;
 let cellWidth;
 let cellHeight;
@@ -27,12 +27,142 @@ function setup() {
   grid = create2dArray(COLS, ROWS);
   //place player in grid
   grid[playerY][playerX] = 9;
+  createObst();
 }
 
 function draw() {
   background(220);
   displayGrid(grid);
+  startRoadStrips();
 }
+
+let roadstrips = [], px = 200, py = 400, score = 0, playing = true;
+
+
+function createObst(){
+  for (let i = 14; i >= -1; i--) {
+    let someArray = [];
+    for (let j = 0; j < 10; j++) {
+      someArray.push(0);
+    }
+    roadstrips.push({
+      y: i * 40,
+      strip: someArray
+    });
+  }
+}
+
+function startRoadStrips() {
+  if (playing){
+    background(220);
+    noStroke();
+    for (let i = 0; i < roadstrips.length; i++) {
+      for (let j = 0; j < 10; j++) {
+        if (roadstrips[i].strip[j] === 0) {
+          fill(255);
+        }
+        if (roadstrips[i].strip[j] === 1) {
+          fill(130);
+        }
+        if (roadstrips[i].strip[j] === 2) {
+          fill(0); // roadstrip img
+        }
+        rect(j * 40, roadstrips[i].y, 40, 40);
+      }
+      roadstrips[i].y += velocity;
+    }
+    fill(255,0,0);
+    ellipse(px+20,py+20,30,30);
+    py+=velocity;
+    if (roadstrips[0].y > height){
+      generateStrip();
+      score+=0.2;
+    }
+    fill(0);
+    text(Math.floor(score), 30,30);
+    velocity = 0.47*frameCount/1000 + 100/py;
+    if(py<height) {
+      playing = false;
+    }
+    else{
+      background(255, 0,0);
+      textAlign(CENTER);
+      fill(255);
+      text("RETRY",width/2, height/2);
+    }
+  }
+}
+
+function generateStrip() {
+  let r = Math.floor(random(3));
+  console.log(r);
+  let a = [];
+  if (r === 0) {
+    for (let i = 0; i < 10; i++) {
+      if (random(2) < 1) {
+        a.push(0);
+      }
+      else {
+        a.push(2);
+      }
+    }
+  }
+  if (r === 1) {
+    for (let i = 0; i < 10; i++) {
+      if (random(5) < 1) {
+        a.push(1);
+      }
+      else {
+        a.push(0);
+      } 
+    }
+  }
+  if (r === 2) {
+    for (let i = 0; i < 10; i++) {
+      if (random(5) < 1){
+        a.push(2);
+      } 
+      else {
+        a.push(0);
+      }
+    }
+  }
+  let last=roadstrips[roadstrips.length-1].y-40;
+  roadstrips.push({
+    y:last,
+    strip: a
+  });
+  roadstrips.shift();
+}
+
+
+// function mouseClicked(){
+//   if(!playing){
+//     setup();
+//   }
+// }
+
+// if(keyCode === UP_ARROW){
+//   if(roadstrips[roadstrips.length-Math.floor(py/40)-1].strip[Math.floor(px/40)]===0 && py>40) {
+//     py -= 40;
+//   }
+// }
+// if(keyCode === DOWN_ARROW){
+//   if(roadstrips[roadstrips.length-Math.floor(py/40)-3].strip[Math.floor(px/40)]===0 && py>40) {
+//     py += 40;
+//   }
+// }
+// if(keyCode === RIGHT_ARROW){
+//   if (roadstrips[roadstrips.length-Math.floor(py/40)-2].strip[Math.floor(px/40)+1]===0 && px<width-40){
+//     px += 40;
+//   }
+// }
+// if(keyCode === LEFT_ARROW){
+//   if (roadstrips[roadstrips.length-Math.floor(py/40)-2].strip[Math.floor(px/40)-1]===0 && px>=40){
+//     px -= 40;
+//   }
+// }
+
 
 function keyPressed() {
   if (keyCode === RIGHT_ARROW) {
@@ -145,116 +275,3 @@ function createRandom2dArray(COLS, ROWS) {
   }
   return emptyArray;
 }
-
-// let roadstrips = [], velocity = 0.6, px = 200, py = 400, score = 0, playing = true;
-
-// function setup() {
-//   createCanvas(400, 600);
-//   createObst();
-// }
-
-// function createObst(){
-//   for (let i = 14; i >= -1; i--) {
-//   someArray = []
-//     for (let j = 0; j < 10; j++) {
-//       someArray.push(0)
-//     }
-//   roadstrips.push({
-//     y: i * 40,
-//     strip: someArray
-//   })
-// }
-// }
-
-// function draw() {
-//   if (playing){
-//     background(220);
-//   	noStroke()
-//   	for (i = 0; i < roadstrips.length; i++) {
-//   		for (j = 0; j < 10; j++) {
-//   			if (roadstrips[i].strip[j] === 0) {
-//   				fill(255)
-//   			}
-//   			if (roadstrips[i].strip[j] === 1) {
-//   				fill(130)
-//   			}
-//   			if (roadstrips[i].strip[j] === 2) {
-//   				fill(0)
-//   			}
-//   			rect(j * 40, roadstrips[i].y, 40, 40)
-//   		}
-//   		roadstrips[i].y += velocity
-//   	}
-//   	fill(255,0,0)
-//   	ellipse(px+20,py+20,30,30);
-//   	py+=velocity
-//   	if (roadstrips[0].y > height){
-//   		generateStrip()
-//       score+=0.2
-//   	}
-//     fill(0)
-//     text(Math.floor(score), 30,30)
-//   	velocity = 0.47*frameCount/1000 + 100/py
-//     if(py>height-40) playing = false
-//   }else{
-//     background(255, 0,0)
-//     textAlign(CENTER)
-//     fill(255)
-//     text('RETRY',width/2, height/2)
-//   }
-// }
-
-// function generateStrip() {
-// 	r = Math.floor(random(3))
-// 	console.log(r)
-// 	a = []
-// 	if (r == 0) {
-// 		for (i = 0; i < 10; i++) {
-// 			if (random(2) < 1) a.push(0)
-// 			else a.push(2)
-// 		}
-// 	}
-// 	if (r == 1) {
-// 		for (i = 0; i < 10; i++) {
-// 			if (random(5) < 1) a.push(1)
-// 			else a.push(0)
-// 		}
-// 	}
-// 	if (r == 2) {
-// 		for (i = 0; i < 10; i++) {
-// 			if (random(5) < 1) a.push(2)
-// 			else a.push(0)
-// 		}
-// 	}
-// 	last=roadstrips[roadstrips.length-1].y-40
-// 	roadstrips.push({
-// 		y:last,
-// 		strip: a
-// 	})
-// 	roadstrips.shift()
-// }
-
-// function keyPressed() {
-// 		if(keyCode == UP_ARROW){
-// 			if(roadstrips[roadstrips.length-Math.floor(py/40)-1].strip[Math.floor(px/40)]==0 && py>40) {py -= 40}
-// 		}
-// 		if(keyCode == DOWN_ARROW){
-// 			if(roadstrips[roadstrips.length-Math.floor(py/40)-3].strip[Math.floor(px/40)]==0 && py>40) {py += 40}
-// 		}
-// 		if(keyCode == RIGHT_ARROW){
-// 			if (roadstrips[roadstrips.length-Math.floor(py/40)-2].strip[Math.floor(px/40)+1]==0 && px<width-40){
-// 				px += 40
-// 			}
-// 		}
-// 		if(keyCode == LEFT_ARROW){
-// 			if (roadstrips[roadstrips.length-Math.floor(py/40)-2].strip[Math.floor(px/40)-1]==0 && px>=40){
-// 				px -= 40
-// 			}
-// 		}
-// }
-
-// function mouseClicked(){
-//   if(!playing){
-//     setup()
-//   }
-// }
